fae0372db53e84eab313da790a288dd2958f7db7
visioned <ajkg11@gmail.com>
2022-11-19 15:31:05 -0800

    card deal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins            font-normal
+            className={`font-poppins font-normal
             cursor-pointer 
             text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} 
             text-white`}
